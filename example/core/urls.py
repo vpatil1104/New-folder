15b0a5c1144b7b88bb3315d4db25f04e219c32aa
vpatil0029 <102610391+vpatil0029@users.noreply.github.com>
2022-12-28 19:28:24 +0530

    first

--- example/core/urls.py
+++ example/core/urls.py
@@ -3,10 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("index", views.index, name="index"),
-    path("q", views.q, name="q"),
-    
-    path("", views.contact, name="contactus"),
+    path("", views.index, name="index"),
+    path("contact", views.contact, name="contactus"),
 
  
 ]
--- example/core/views.py
+++ example/core/views.py
@@ -2,14 +2,14 @@
 
 # Create your views here.
 from django.shortcuts import render
-#from core.models import Contact
+from core.models import Contact
 from datetime import datetime
 from django.db import models
 from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 
-def index(request):
+def contact(request):
    
     return render(request,'contactus.html')
 
@@ -17,13 +17,13 @@ def q(request):
        
     return render(request,'swiper.html')
 
-def contact(request):
-    # if request.method =='POST':
-    #     fname = request.POST.get('fname')
-    #     lname = request.POST.get('lname')
-    #     email = request.POST.get('email')
-    #     phone = request.POST.get('phone')
-    #     message = request.POST.get('message')
-    #     contact=Contact(fname=fname,lname=lname,email=email,phone=phone,message=message)
-    #     contact.save()
+def index(request):
+    if request.method =='POST':
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        contact=Contact(fname=fname,lname=lname,email=email,phone=phone,message=message)
+        contact.save()
     return render(request,'index.html')
